002150a390883a7b63896979e08fbe82cf120d2d
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-01-10 22:16:42 -0400

    Added Exercise 1.2

--- src/test/java/com/github/gabrielbb/algorithms/Chapter1Test.java
+++ src/test/java/com/github/gabrielbb/algorithms/Chapter1Test.java
@@ -1,9 +1,13 @@
 package com.github.gabrielbb.algorithms;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.github.gabrielbb.algorithms.assignments.chapter1.IsUnique;
+import com.github.gabrielbb.algorithms.assignments.chapter1.Permutation;
+
 import org.junit.Test;
 
 /**
@@ -15,5 +19,13 @@ public class Chapter1Test {
     public void testIsUnique() {
         assertTrue(IsUnique.isUnique("Carote"));
         assertFalse(IsUnique.isUnique("Casa"));
+    }
+
+    @Test
+    public void testPermutation() {
+        assertTrue(Permutation.isPermutation("Casa", "asaC"));
+        assertTrue(Permutation.isPermutation("KELOKE", "KKOLEE"));
+        assertFalse(Permutation.isPermutation("messaka[p[askdge", "sadasdsadsdasd"));
+        assertFalse(Permutation.isPermutation("", "  "));
     }
 }
