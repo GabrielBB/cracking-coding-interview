e7525915668137dc5d783fc1946faa7d552c9b43
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-01-12 21:10:34 -0400

    Changing StringRotation solution to the one in the book because is way cleaner

--- src/main/java/com/github/gabrielbb/algorithms/assignments/chapter1/StringRotation.java
+++ src/main/java/com/github/gabrielbb/algorithms/assignments/chapter1/StringRotation.java
@@ -3,35 +3,6 @@ package com.github.gabrielbb.algorithms.assignments.chapter1;
 public class StringRotation {
 
     public static boolean isRotation(String original, String second) {
-
-        if (original == null || second == null || original.length() != second.length()) {
-            return false;
-        }
-
-        if(original.isEmpty() && second.isEmpty()) {
-            return true;
-        }
-
-        char[] oChars = original.toCharArray();
-        char[] sChars = second.toCharArray();
-
-        int oIndex = 0;
-        int sIndex = second.indexOf(oChars[oIndex]);
-
-        if (sIndex < 0) {
-            return false;
-        }
-
-        while (sIndex < sChars.length) {
-            if (oChars[oIndex] != sChars[sIndex]) {
-                return false;
-            }
-
-            sIndex++;
-            oIndex++;
-        }
-
-        return oIndex == original.length() - 1 // Reached the end and everything was the same
-                || original.substring(oIndex).equals(second.substring(0, original.length() - oIndex));
+        return original.length() == second.length() && (original + original).contains(second);
     }
 }
