dc0172ea47c33e24b6a89265bb2b7272a3c699ac
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-01-13 23:58:07 -0400

    Simplying 2.3

--- src/main/java/com/github/gabrielbb/algorithms/assignments/chapter2/MiddleNodeDeletion.java
+++ src/main/java/com/github/gabrielbb/algorithms/assignments/chapter2/MiddleNodeDeletion.java
@@ -5,18 +5,7 @@ import com.github.gabrielbb.structures.LinkedList.Node;
 public class MiddleNodeDeletion {
 
     public static void deleteNode(Node<Integer> node) {
-
-        Node<Integer> currentNode = node;
-
-        while (currentNode != null) {
-
-            currentNode.data = currentNode.next.data;
-
-            if(currentNode.next.next == null) {
-                currentNode.next = null;
-            }
-
-            currentNode = currentNode.next;
-        }
+        node.data = node.next.data;
+        node.next = node.next.next;
     }
 }
