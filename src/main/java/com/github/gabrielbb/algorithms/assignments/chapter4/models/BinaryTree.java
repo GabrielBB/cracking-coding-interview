e9fe17ca0a4c5e6a2741a0a4d35af36e9f2ae258
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-02-11 16:59:20 -0400

    Starting to make exercise 4.11

--- src/main/java/com/github/gabrielbb/algorithms/assignments/chapter4/models/BinaryTree.java
+++ src/main/java/com/github/gabrielbb/algorithms/assignments/chapter4/models/BinaryTree.java
@@ -3,9 +3,29 @@ package com.github.gabrielbb.algorithms.assignments.chapter4.models;
 public class BinaryTree<T> {
 
     public Node<T> root;
+    private int depth = 1;
+    private int nodesInDepth = 0;
 
     public BinaryTree(Node<T> root) {
         this.root = root;
+    }
+
+    public void insert(Node<T> node) {
+        if (nodesInDepth == (depth * 2)) {
+            depth++;
+            nodesInDepth = 0;
+        }
+
+        insert(node, 1);
+    }
+
+    private boolean insert(Node<T> node, int depth) {
+
+        if (depth == this.depth) {
+            
+        }
+
+        return false;
     }
 
     public static class Node<T> {
