a8d77bdd3a99fb64270ce15d8eed62ff84dd3554
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-04-11 16:52:08 -0400

    Comitting 8.2

---
+++ src/test/java/com/github/gabrielbb/algorithms/Chapter5Test.java
@@ -0,0 +1,26 @@
+package com.github.gabrielbb.algorithms;
+
+import com.github.gabrielbb.algorithms.assignments.chapter5.RobotGrid;
+import com.github.gabrielbb.algorithms.assignments.chapter5.RobotGrid.Move;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Chapter5Test {
+
+    @Test
+    public void testRobotGrid() {
+
+        boolean[][] grid = new boolean[3][3];
+
+        Move move = RobotGrid.getRobotPath(grid, 0, 0);
+
+        while(move != null) {
+            System.out.println(move.direction);
+            move = move.next;
+        }
+    }
+
+}
