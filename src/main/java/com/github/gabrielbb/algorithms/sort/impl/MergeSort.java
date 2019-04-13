014be91d51e36a26baf3e28037e3cae4cd7c2589
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-04-13 19:52:29 -0400

    Comitting MergeSort

---
+++ src/main/java/com/github/gabrielbb/algorithms/sort/impl/MergeSort.java
@@ -0,0 +1,51 @@
+package com.github.gabrielbb.algorithms.sort.impl;
+
+import com.github.gabrielbb.algorithms.sort.Sortable;
+
+public class MergeSort implements Sortable {
+
+    @Override
+    public void sort(int[] array) {
+        mergeSort(array, array.clone(), 0, array.length - 1);
+    }
+
+    private void mergeSort(int[] array, int[] helper, int start, int end) {
+
+        if (start < end) {
+            int middle = (start + end) / 2;
+            mergeSort(array, helper, start, middle);
+            mergeSort(array, helper, middle + 1, end);
+
+            merge(array, helper, start, middle, end);
+        }
+
+    }
+
+    void merge(int[] array, int[] helper, int low, int middle, int end) {
+
+        int current = low;
+        int helperLeft = low;
+        int helperRight = middle + 1;
+
+        /*
+         * Iterate through helper array. Compare the left and right half, copying back
+         * the smaller element from the two halves into the original array.
+         */
+        while (helperLeft <= middle && helperRight <= end) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                array[current] = helper[helperLeft];
+                helperLeft++;
+            } else {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        /* Copy the rest of the left side of the array into the target array */
+        int remaining = middle - helperLeft;
+        for (int i = 0; i <= remaining; i++) {
+            array[current + i] = helper[helperLeft + i];
+        }
+    }
+}
--- src/test/java/com/github/gabrielbb/algorithms/SortTest.java
+++ src/test/java/com/github/gabrielbb/algorithms/SortTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import com.github.gabrielbb.algorithms.sort.Sortable;
 import com.github.gabrielbb.algorithms.sort.impl.BubbleSort;
 import com.github.gabrielbb.algorithms.sort.impl.InsertionSort;
+import com.github.gabrielbb.algorithms.sort.impl.MergeSort;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,9 +22,13 @@ public class SortTest {
         testSort(new InsertionSort());
     }
 
+    @Test
+    public void testMergeSort() {
+        testSort(new MergeSort());
+    }
 
     private void testSort(Sortable sortable) {
-        final int[] array = new int[] { 199, 25, 10, 178, 125, 12, 47, 122, 121 };
+        final int[] array = new int[] { 1, 5, 8, 0, 4, 6 };
 
         final int[] array2 = array.clone();
         Arrays.sort(array2);
